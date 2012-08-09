36455dd51a42ab745eef2927e05bb9412dad37d0
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-08-09 17:01:18 +0200

    settings: sensor and translation cleanup

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -41,8 +41,8 @@ public class DeviceSettings extends FragmentActivity {
     public static final String KEY_MDNIE_OUTDOOR = "mdnie_outdoor";
     public static final String KEY_PANEL_GAMMA = "panel_gamma";
     public static final String KEY_HSPA = "hspa";
-    public static final String KEY_USE_GYRO_CALIBRATION = "use_gyro_calibration";
-    public static final String KEY_CALIBRATE_GYRO = "calibrate_gyro";
+    public static final String KEY_USE_ACCELEROMETER_CALIBRATION = "use_accelerometer_calibration";
+    public static final String KEY_CALIBRATE_ACCELEROMETER = "calibrate_accelerometer";
     public static final String KEY_TOUCHSCREEN_SENSITIVITY = "touchscreen_sensitivity";
     public static final String KEY_TOUCHKEY_LIGHT = "touchkey_light";
     public static final String KEY_TOUCHKEY_TIMEOUT = "touchkey_timeout";
--- DeviceSettings/src/com/cyanogenmod/settings/device/SensorsFragmentActivity.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/SensorsFragmentActivity.java
@@ -35,7 +35,7 @@ public class SensorsFragmentActivity extends PreferenceFragment {
     private static final String PREF_ENABLED = "1";
     private static final String TAG = "GalaxyS2Settings_Sensors";
 
-    private static final String FILE_USE_GYRO_CALIB = "/sys/class/sec/gsensorcal/calibration";
+    private static final String FILE_USE_ACCELEROMETER_CALIB = "/sys/class/sec/gsensorcal/calibration";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,15 +53,15 @@ public class SensorsFragmentActivity extends PreferenceFragment {
 
         Log.w(TAG, "key: " + key);
 
-        if (key.compareTo(DeviceSettings.KEY_USE_GYRO_CALIBRATION) == 0) {
+        if (key.compareTo(DeviceSettings.KEY_USE_ACCELEROMETER_CALIBRATION) == 0) {
             boxValue = (((CheckBoxPreference)preference).isChecked() ? "1" : "0");
-            Utils.writeValue(FILE_USE_GYRO_CALIB, boxValue);
-        } else if (key.compareTo(DeviceSettings.KEY_CALIBRATE_GYRO) == 0) {
+            Utils.writeValue(FILE_USE_ACCELEROMETER_CALIB, boxValue);
+        } else if (key.compareTo(DeviceSettings.KEY_CALIBRATE_ACCELEROMETER) == 0) {
             // when calibration data utilization is disablen and enabled back,
             // calibration is done at the same time by driver
-            Utils.writeValue(FILE_USE_GYRO_CALIB, "0");
-            Utils.writeValue(FILE_USE_GYRO_CALIB, "1");
-            Utils.showDialog((Context)getActivity(), "Calibration done", "The gyroscope has been successfully calibrated!");
+            Utils.writeValue(FILE_USE_ACCELEROMETER_CALIB, "0");
+            Utils.writeValue(FILE_USE_ACCELEROMETER_CALIB, "1");
+            Utils.showDialog((Context)getActivity(), getString(R.string.accelerometer_dialog_head), getString(R.string.accelerometer_dialog_message));
         }
 
         return true;
@@ -73,12 +73,12 @@ public class SensorsFragmentActivity extends PreferenceFragment {
 
     public static void restore(Context context) {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-        boolean gyroCalib = sharedPrefs.getBoolean(DeviceSettings.KEY_USE_GYRO_CALIBRATION, true);
+        boolean accelerometerCalib = sharedPrefs.getBoolean(DeviceSettings.KEY_USE_ACCELEROMETER_CALIBRATION, true);
 
-        // When use gyro calibration value is set to 1, calibration is done at the same time, which
+        // When use accelerometer calibration value is set to 1, calibration is done at the same time, which
         // means it is reset at each boot, providing wrong calibration most of the time at each reboot.
         // So we only set it to "0" if user wants it, as it defaults to 1 at boot
-        if (!gyroCalib)
-            Utils.writeValue(FILE_USE_GYRO_CALIB, "0");
+        if (!accelerometerCalib)
+            Utils.writeValue(FILE_USE_ACCELEROMETER_CALIB, "0");
     }
 }
