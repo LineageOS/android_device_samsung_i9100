5366c68670140bc311b06ab77c26a7e628a7bf8c
codeworkx <codeworkx@cyanogenmod.org>
2012-12-02 16:01:57 +0100

    i9100: add bluetooth build cfg
    Change-Id: Id905163db2e8cb2369aa27c619fbda36ad5b2643

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@
 
 TARGET_BOARD_INFO_FILE := device/samsung/i9100/board-info.txt
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9100/bluetooth
+
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4210
 TARGET_KERNEL_CONFIG := cyanogenmod_i9100_defconfig
