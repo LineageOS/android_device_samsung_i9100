24b759b2a893022379cb46eb06185f231234d92f
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-21 18:05:47 +0200

    i9100: switch to smdk4412 kernel
    Change-Id: I9f614c4a23503c8b14daebf7996b076fcd264db8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,11 +25,11 @@ TARGET_BOARD_INFO_FILE := device/samsung/i9100/board-info.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9100/bluetooth
 
 # Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/samsung/smdk4210
+TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_i9100_defconfig
 
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/i9100/overlay/include
-EXYNOS4210_ENHANCEMENTS := true
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T
 
