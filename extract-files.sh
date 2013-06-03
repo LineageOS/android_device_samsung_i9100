2deaf66c4646e6bca85cd1900798b8bb18cc900c
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-06-03 14:08:27 +0200

    i9100: fix extract script
    Change-Id: I64c3bf857027bddb075a156cdfcc9b0982740157

--- extract-files.sh
+++ extract-files.sh
@@ -48,12 +48,7 @@ done
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \\
-    vendor/__VENDOR__/__DEVICE__/proprietary/system/lib/libril.so:obj/lib/libril.so \\
-    vendor/__VENDOR__/__DEVICE__/proprietary/system/lib/libsecril-client.so:obj/lib/libsecril-client.so
-
-PRODUCT_COPY_FILES += \\
 EOF
 
 LINEEND=" \\"
