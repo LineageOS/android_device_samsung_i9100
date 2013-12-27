7d214d9467baf2a1908d9a349b1868d45e3abcff
David Van Cleef <vancleef@gmail.com>
2013-12-27 10:14:41 +0900

    Add asserts to allow install on an SC-02C handset (japanese model number for i9100).  Hardware is 99% identical to i9100.
    Change-Id: I8a5704d5b6225bb4cb20901e9be7b16eaf9b987e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_i9100_defconfig
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T
+TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T,SC-02C
 
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/i9100/BoardConfigVendor.mk
