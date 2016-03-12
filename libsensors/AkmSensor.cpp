f1c349c917608090e0272e5b3c07d08dc602e741
rINanDO <drjisakh@gmail.com>
2016-03-12 22:00:37 +0100

    i9100: libsensors: use CLOCK_BOOTTIME, not CLOCK_MONOTONIC.
    This fixes rotation issues without using the hack 'config_useSystemClockforRotationSensor'.
    
    Change-Id: I9b6afc46b4dfabc69e4d4ad215de7a9f2ea15347

--- libsensors/AkmSensor.cpp
+++ libsensors/AkmSensor.cpp
@@ -253,11 +253,10 @@ int AkmSensor::readEvents(sensors_event_t* data, int count)
             processEvent(event->code, event->value);
             mInputReader.next();
         } else if (type == EV_SYN) {
-            int64_t time = timevalToNano(event->time);
             for (int j=0 ; count && mPendingMask && j<numSensors ; j++) {
                 if (mPendingMask & (1<<j)) {
                     mPendingMask &= ~(1<<j);
-                    mPendingEvents[j].timestamp = time;
+                    mPendingEvents[j].timestamp = getTimestamp();
                     if (mEnabled & (1<<j)) {
                         *data++ = mPendingEvents[j];
                         count--;
