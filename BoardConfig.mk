c5d796dee9a519ca797b53288a92c79a97c33ee0
Wolfram Liebchen <achalmgucker@googlemail.com>
2016-03-26 00:31:21 +0100

    [3/3] Disable touchkeys and enable navbar
    The touchkeys can be disabled and remain completely invisible.
    This allows the navbar to be enabled/disabled via settings.
    
    Change-Id: I093b50356351020c6f1f98639e44e43950dad499

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@
 
 TARGET_BOARD_INFO_FILE := device/samsung/i9100/board-info.txt
 
+# CM overlay to  frameworks/opt/hardware/src/
+BOARD_HARDWARE_CLASS += device/samsung/i9100/cmhw
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9100/bluetooth
 
