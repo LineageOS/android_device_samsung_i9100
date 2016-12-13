e4ceb540f48be4b2a0f2a102dd705ba1bdbbf9fd
Dominggoes Isakh <drjisakh@gmail.com>
2016-12-13 21:03:46 +0100

    i9100: Switch to common_mini_phone
    common_full_phone doesn't fit anymore in our 512MB partition.
    
    Change-Id: Ic93ea63909bd32dafbe1be0b83b8f38970aab48f

--- cm.mk
+++ cm.mk
@@ -22,7 +22,7 @@ TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9100/full_i9100.mk)
