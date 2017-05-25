30294a6bc76d838b2dd1d9cb93a8cc38d6010ee0
Dominggoes Isakh <drjisakh@gmail.com>
2017-05-25 08:44:36 +0200

    i9100: Enable variable touchkey button brightness
    Change-Id: I99f557b4875bbc7169c383b59af65aae44d5b2f1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,9 @@ TARGET_BOARD_INFO_FILE := device/samsung/i9100/board-info.txt
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9100/bluetooth
 
+# Touchkeys
+BOARD_HAS_VAR_TOUCHKEY_BRIGHTNESS := true
+
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := lineageos_i9100_defconfig
