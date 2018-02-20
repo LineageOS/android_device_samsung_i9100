e83bd4d0950031d7fd2587d588a1f1fdd3428a37
Dominggoes Isakh <drjisakh@gmail.com>
2018-02-20 20:57:19 +0100

    i9100: Add libstlport
    Change-Id: I932220632dbfb568b9d2451620477cfb4aa06e92

---
+++ libstlport/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libstlport
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES := $(LOCAL_MODULE).so
+
+include $(BUILD_PREBUILT)
+
